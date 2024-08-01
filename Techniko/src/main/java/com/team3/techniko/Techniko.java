055335f6312b77fb8d1616d78b7a08886eaa6d9f
SpyrosK <spiroskat18@gmail.com>
2024-08-01 13:08:43 +0300

    commit uno

--- Techniko/src/main/java/com/team3/techniko/Techniko.java
+++ Techniko/src/main/java/com/team3/techniko/Techniko.java
@@ -1,8 +1,15 @@
 package com.team3.techniko;
 
+import com.team3.techniko.model.Admin;
+import com.team3.techniko.model.Property;
 import com.team3.techniko.model.PropertyOwner;
+import com.team3.techniko.model.PropertyRepair;
+import com.team3.techniko.model.enums.Status;
 import com.team3.techniko.service.AdminService;
 import com.team3.techniko.service.PropertyOwnerService;
+import com.team3.techniko.service.PropertyRepairService;
+import java.util.Date;
+import java.util.List;
 import javax.persistence.*;
 
 public class Techniko {
@@ -10,16 +17,18 @@ public class Techniko {
     public static void main(String[] args) {
         
         
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Technikon");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Technikon");
+//        EntityManager entityManager = entityManagerFactory.createEntityManager();
+//
+//        entityManager.getTransaction().begin();
+//        entityManager.getTransaction().commit();
+//
+//        entityManager.close();
+//        entityManagerFactory.close();
+////        PropertyOwnerService propertyOwnerService = new PropertyOwnerService();
+////        propertyOwnerService.savePropertyOwner(new PropertyOwner("324123","Stefanos",
+////                "Ountrakis","Kolimvari","69324234234","sountrakis@gmail.com","Stevoun","1234"));
+////        System.out.println(propertyOwnerService.getAllPropertyOwners());
 
-        entityManager.getTransaction().begin();
-        entityManager.getTransaction().commit();
-
-        entityManager.close();
-        entityManagerFactory.close();
-//        PropertyOwnerService propertyOwnerService = new PropertyOwnerService();
-//        propertyOwnerService.savePropertyOwner(new PropertyOwner("324123","Stefanos",
-//                "Ountrakis","Kolimvari","69324234234","sountrakis@gmail.com","Stevoun","1234"));
-//        System.out.println(propertyOwnerService.getAllPropertyOwners());
-    }}
+        
+}}
