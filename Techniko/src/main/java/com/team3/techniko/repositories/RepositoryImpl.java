f5ec4d207b832be22c47cf19f4a0b6ebdfaa88ee
Stefanos Ountrakis <sountrakis@gmail.com>
2024-08-02 13:58:24 +0300

    All repos to one

--- Techniko/src/main/java/com/team3/techniko/repositories/RepositoryImpl.java
+++ Techniko/src/main/java/com/team3/techniko/repositories/RepositoryImpl.java
@@ -1,6 +1,5 @@
 package com.team3.techniko.repositories;
 
-import com.team3.techniko.model.PropertyOwner;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -9,33 +8,38 @@ import javax.persistence.TypedQuery;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class PropertyOwnerRepository implements Repository<PropertyOwner, Long>{
-    private final EntityManager entityManager;
+public class RepositoryImpl<T> implements Repository<T, Long> {
 
-    public PropertyOwnerRepository(EntityManager entityManager) {
+    private final EntityManager entityManager;
+    private final Class<T> entityClass;
+
+    public RepositoryImpl(EntityManager entityManager, Class<T> entityClass) {
         this.entityManager = entityManager;
+        this.entityClass = entityClass;
     }
 
     @Override
-    public Optional<PropertyOwner> findById(Long id) {
+    public Optional<T> findById(Long id) {
         try {
             entityManager.getTransaction().begin();
-            PropertyOwner t = entityManager.find(getEntityClass(), id);
+            T t = entityManager.find(entityClass, id);
             entityManager.getTransaction().commit();
             return Optional.of(t);
         } catch (Exception e) {
             log.debug("An exception occured");
         }
-        return Optional.empty();  }
+        return Optional.empty();
+    }
 
     @Override
-    public List<PropertyOwner> findAll() {
-       TypedQuery<PropertyOwner> query = entityManager.createQuery("from " + getEntityClassName(), getEntityClass());
-        return query.getResultList();    }
+    public List<T> findAll() {
+        TypedQuery<T> query = entityManager.createQuery("from " + entityClass.getName(), entityClass);
+        return query.getResultList();
+    }
 
     @Override
-    public Optional<PropertyOwner> save(PropertyOwner t) {
-          try {
+    public Optional<T> save(T t) {
+        try {
             entityManager.getTransaction().begin();
             entityManager.persist(t);
             entityManager.getTransaction().commit();
@@ -43,11 +47,12 @@ public class PropertyOwnerRepository implements Repository<PropertyOwner, Long>{
         } catch (Exception e) {
             log.debug("An exception occured");
         }
-        return Optional.empty();    }
+        return Optional.empty();
+    }
 
     @Override
     public boolean deleteById(Long id) {
-        PropertyOwner persistentInstance = entityManager.find(getEntityClass(), id);
+        T persistentInstance = entityManager.find(entityClass, id);
         if (persistentInstance != null) {
 
             try {
@@ -60,29 +65,19 @@ public class PropertyOwnerRepository implements Repository<PropertyOwner, Long>{
             }
             return true;
         }
-        return false;   
-    }
-    
-    
-    private Class<PropertyOwner> getEntityClass() {
-        return PropertyOwner.class;
+        return false;
     }
 
-    private String getEntityClassName() {
-        return PropertyOwner.class.getName();
-        
-    }
-    
-     @Override
-    public List<PropertyOwner> findByUserId(Long userId) {
-        TypedQuery<PropertyOwner> query = entityManager.createQuery("from " + getEntityClassName() + " where ownerId = :ownerId", getEntityClass());
+    @Override
+    public List<T> findByUserId(Long userId) {
+        TypedQuery<T> query = entityManager.createQuery("from " + entityClass.getName() + " where ownerId = :ownerId", entityClass);
         query.setParameter("ownerId", userId);
         return query.getResultList();
     }
 
     @Override
-    public List<PropertyOwner> findByDateRange(Date startDate, Date endDate) {
-        TypedQuery<PropertyOwner> query = entityManager.createQuery("from " + getEntityClassName() + " where date >= :startDate and date <= :endDate", getEntityClass());
+    public List<T> findByDateRange(Date startDate, Date endDate) {
+        TypedQuery<T> query = entityManager.createQuery("from " + entityClass.getName() + " where date >= :startDate and date <= :endDate", entityClass);
         query.setParameter("startDate", startDate);
         query.setParameter("endDate", endDate);
         return query.getResultList();
