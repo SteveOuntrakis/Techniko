962eb444aeb0edc10751f20c4240284c87c17a40
Panagiotis Kostopoulos <pankos2121@gmail.com>
2024-08-02 13:11:40 +0300

    add Property repo

--- Techniko/src/main/java/com/team3/techniko/Techniko.java
+++ Techniko/src/main/java/com/team3/techniko/Techniko.java
@@ -3,6 +3,7 @@ package com.team3.techniko;
 import com.team3.techniko.model.PropertyOwner;
 import javax.persistence.*;
 import com.team3.techniko.repositories.PropertyOwnerRepository;
+import com.team3.techniko.repositories.PropertyRepository;
 
 public class Techniko {
 
@@ -19,5 +20,8 @@ public class Techniko {
         ownerRepository.save(new PropertyOwner("324123","Stefanos",
                       "Ountrakis","Kolimvari","69324234234","sountrakis@gmail.com","Stevoun","1234"));
         System.out.println(ownerRepository.findAll());
+      
+        PropertyRepository propertyRepository = new PropertyRepository(entityManagerFactory);
+    
     }
 }
